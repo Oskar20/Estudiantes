f9fc56fd520f03553f8469903a22f7bcf9ee6f22
Oscar Pallares <oscarap20@gmail.com>
2020-05-04 20:52:19 -0500

    Proyecto actualizado 1ra versión

--- Data/Properties/Settings.Designer.cs
+++ Data/Properties/Settings.Designer.cs
@@ -26,7 +26,6 @@ namespace Data.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=OSKARPC;Initial Catalog=Estudiantes;User ID=sa;Password=dbuser")]
         public string EstudiantesConnectionString {
             get {
                 return ((string)(this["EstudiantesConnectionString"]));
